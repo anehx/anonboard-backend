b4503866399212eb847e526d3ff844e79177febf
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2016-02-23 15:03:24 +0100

    Use postgresql

--- Makefile
+++ Makefile
@@ -1,5 +1,14 @@
-test:
+install:
+	@pip install -r requirements.txt
+
+test: start-db
 	@python manage.py test --keepdb
 
-start:
+migrate:
+	@python manage.py migrate
+
+start-db:
+	@docker-compose up -d db
+
+start: start-db
 	@python manage.py runserver
--- anonboard/settings.py
+++ anonboard/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'anonboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'NAME':     'anonboard',
+        'USER':     'anonboard',
+        'PASSWORD': 'anonboard',
+        'HOST':     'localhost'
     }
 }
 
